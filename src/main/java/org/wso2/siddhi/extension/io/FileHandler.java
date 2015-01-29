7ac3a2d9ab425ce8e29848ccc283b32bc81a7ea2
Tharindu Munasinghe <munasinghetharindu@gmail.com>
2015-01-29 14:50:01 +0530

    code is refactored and exections are handled

--- src/main/java/org/wso2/siddhi/extension/io/FileHandler.java
+++ src/main/java/org/wso2/siddhi/extension/io/FileHandler.java
@@ -1,51 +1,73 @@
+/*
+ * Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.wso2.siddhi.extension.io;
 
-import org.wso2.siddhi.extension.util.ScriptExecutor;
 import org.apache.log4j.Logger;
+import org.wso2.siddhi.extension.DataWranglerTransformProcessor;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-
-/******************************************************************************
+/**
  * FileHandler Class will read resource files and return the contents as  texts
  * Created by tharindu on 1/21/15.
- ******************************************************************************/
+ */
 
 public class FileHandler {
 
-    //Attributes .............................................................
-    private static Logger logger = Logger.getLogger(FileHandler.class);
+	//Attributes .............................................................
+	private static Logger logger = Logger.getLogger(FileHandler.class);
 
+	/**
+	 * This method read the content of a file inside the resouces folder( dw.js, lib/*)
+	 *
+	 * @param fileName : Name of the file you want to read (within the resources folder)
+	 * @return : content of the file as a String
+	 */
 
-    //Methods ................................................................
+	public static String readScript(String fileName) throws IOException {
+		InputStream in = null;
+		BufferedReader reader = null;
+		StringBuilder out = new StringBuilder();
+		try {
+			in = DataWranglerTransformProcessor.class.getResourceAsStream(fileName);
+			reader = new BufferedReader(new InputStreamReader(in));
 
-    /**
-     * This method read the content of a file inside the resouces folder( dw.js, lib/*)
-     * @param fileName : Name of the file you want to read (within the resources folder)
-     * @return : content of the file as a String
-     */
-    public static  String readScript(String fileName){
+			String line;
 
-        StringBuilder out=null;
-        try {
+			while ((line = reader.readLine()) != null) {
+				out.append(line).append("\n");
+			}
+			logger.debug(fileName + " is added Successfully");
+			return out.toString();
 
-            InputStream in=ScriptExecutor.class.getResourceAsStream(fileName);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-            out             = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                out.append(line+"\n");
-            }
-            reader.close();
-            logger.info(fileName + " is added Successfully");
+		} catch (IOException e) {
 
-        }catch (Exception e){
+			logger.error("Error occurred in FileHandler.readScript(" + fileName + ")\n" + e);
+			throw e;
+		} finally {
+			if (reader != null)
+				reader.close();
+			if (in != null)
+				in.close();
+		}
 
-             logger.error(e.getMessage());
-        }
-        return out.toString();
-    }
-
+	}
 
 }
--- src/main/java/org/wso2/siddhi/extension/io/RegistryClient.java
+++ src/main/java/org/wso2/siddhi/extension/io/RegistryClient.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.wso2.siddhi.extension.io;
 
 import org.apache.log4j.Logger;
@@ -6,103 +22,96 @@ import org.wso2.carbon.context.RegistryType;
 import org.wso2.carbon.registry.api.Registry;
 import org.wso2.carbon.registry.api.RegistryException;
 import org.wso2.carbon.registry.api.Resource;
+import org.wso2.siddhi.extension.util.Parameters;
 
-
-/*********************************************************************
+/**
  * This class handle the interactions with WSO2 CEP registry
  * Created by tharindu on 1/20/15.
- *********************************************************************/
+ */
 
 public class RegistryClient {
 
-    //Attributes ..................................................................
+	//Attributes ..................................................................
 
-    private static Logger logger = Logger.getLogger(RegistryClient.class);
-    private static final String BASEURL="/repository/components/org.wso2.cep.wrangler/";
+	private static Logger logger = Logger.getLogger(RegistryClient.class);
+	private Registry registry;
 
+	public RegistryClient() {
+		CarbonContext cCtx = CarbonContext.getCurrentContext();
+		String registryType = RegistryType.SYSTEM_GOVERNANCE.toString();
+		registry = cCtx.getRegistry(RegistryType.valueOf(registryType));
+	}
 
+	//Method ......................................................................
 
-    //Method ......................................................................
+	/**
+	 * This method return the JS script for given configuration
+	 *
+	 * @param name : name of the Script Configuration
+	 * @return : content of the JS code as a string
+	 */
+	public String getScript(String name) throws RegistryException {
+		try {
+			String output = getText(name, Parameters.SCRIPT_NAME);
 
-    /**
-     * This method return the JS script for given configuration
-     *
-     * @param name : name of the Script Configuration
-     * @return : content of the JS code as a string
-     */
-    public String getScript(String name)
-    {
-        String output=getText(name,"script.js");
-        if(output!=null)
-        {
-            return output;
-        }
-        else{
-            logger.error("script output is null");
-            return null;
-        }
+			logger.info("Script " + name + "  is read successfully ");
+			return output;
 
-    }
+		} catch (RegistryException ex) {
+			logger.error("Exception occurred in RegistryClient.getOutputStreamDefinition(" + name +
+			             ")\n" + ex);
+			throw ex;
+		}
 
+	}
 
+	/**
+	 * This method read the output stream definition of a given configuration name
+	 *
+	 * @param name: name of the Configuration in registry
+	 * @return : content of the definition file as a String
+	 */
+	public String getOutputStreamDefinition(String name) throws RegistryException {
+		try {
+			String output = getText(name, Parameters.CONFIG_NAME);
 
-    /**
-     * This method read the output stream definition of a given configuration name
-     *
-     * @param name: name of the Configuration in registry
-     * @return : content of the definition file as a String
-     */
-    public String getOutputStreamDefinition(String name)
-    {
-        String output=getText(name,"config.json");
-        if(output!=null)
-        {
-            logger.info("outputStream Definition is read successfully ");
-            return output;
-        }
-        else{
-            logger.error("output definition is null");
-            return null;
-        }
-    }
+			logger.info("outputStream Definition " + name + " is read successfully ");
+			return output;
 
+		} catch (RegistryException ex) {
+			logger.error("Exception occurred in RegistryClient.getOutputStreamDefinition(" + name +
+			             ")\n" + ex);
+			throw ex;
+		}
+	}
 
+	/**
+	 * This is a generic method to read a file from the registry
+	 *
+	 * @param folderName : name of the folder (which is same as configuration name)
+	 * @param fileName   : config.json | script.js
+	 * @return : content of the file as a String
+	 * @throws RegistryException
+	 */
+	private String getText(String folderName, String fileName) throws RegistryException {
 
-    /**
-     * This is a generic method to read a file from the registry
-     *
-     * @param folderName : name of the folder (which is same as configuration name)
-     * @param fileName : config.json | script.js
-     * @return : content of the file as a String
-     */
-    public String getText(String folderName,String fileName)
-    {
+		try {
+			String contentPath = Parameters.BASE_URL + folderName + "/" + fileName;
+			logger.debug("Retrieving path :" + contentPath);
+			Resource resource = registry.get(contentPath);
 
-        CarbonContext cCtx = CarbonContext.getCurrentContext();
-        Registry registry = cCtx.getRegistry(RegistryType.SYSTEM_CONFIGURATION);
-        String registryType = RegistryType.SYSTEM_GOVERNANCE.toString();
-        if(registryType != null) {
-            registry = cCtx.getRegistry(RegistryType.valueOf(registryType));
-        }
+			byte[] streamOfBytes = (byte[]) resource.getContent();
+			String content = new String(streamOfBytes);
+			logger.debug("Content of the " + contentPath + " is :\n" + content);
 
+			return content;
 
-        try {
-            logger.error("Retrieving path :" + BASEURL + folderName + "/" + fileName);
-            Resource res=registry.get(BASEURL + folderName + "/" + fileName);
-            logger.error(new String((byte[]) res.getContent()));
-
-            return new String((byte[])res.getContent());
-
-        } catch (RegistryException e) {
-            logger.error("Error in Registry client" + e.getMessage());
-
-        }
-        catch (Exception e){
-            logger.error("Error in Registry client"+e.getMessage());
-        }
-
-        return  null;
-    }
-
+		} catch (RegistryException e) {
+			logger.error(
+					"Exception occurred in RegistryClient.getText(" + folderName + "," + fileName +
+					") \n" + e);
+			throw e;
+		}
+	}
 
 }
